81dbb2d97da6ddb8bcadf025108a76e83785abff
SMIT N DOSHI <smitdoshi007@gmail.com>
2016-08-01 20:41:24 -0400

    Added few thing to javascript

--- client/madLib.js
+++ client/madLib.js
@@ -1,21 +1,6 @@
 let exclamation, adverb, noun, verb;
 let madLib;
-/*
-Template.body.helpers({
-	exclamation: function(){
-		return Session.get('exclamation');
-	},
-	adverb: function(){
-		return Session.get('adverb');
-	},
-	noun: function(){
-		return Session.get('noun');
-	},
-	verb: function(){
-		return Session.get('verb');
-	},
-});
-*/
+
 Template.body.events({
 	'click #submitButton': function(event, template){
 		event.preventDefault();
@@ -27,5 +12,10 @@ Template.body.events({
 
 		console.log(exclamation + "! you say, as you " +adverb + 
 			" jump onto your " + noun + " and " + verb + " into the sunset.");
+
+		madLib = exclamation + "! you say, as you " +adverb + 
+			" jump onto your " + noun + " and " + verb + " into the sunset.";
+		$('#createMadLib').append(madLib);
+		$('#createMadLib').css("color","#0000ff");
 	}
 });
