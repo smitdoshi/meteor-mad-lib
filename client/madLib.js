6434bfa8e6d62b3b0850a31ffa266c882592f28a
dancastillo <dancastillo99@gmail.com>
2016-08-01 20:29:28 -0400

    console.log

--- client/madLib.js
+++ client/madLib.js
@@ -1,7 +1,31 @@
-
+let exclamation, adverb, noun, verb;
+let madLib;
+/*
+Template.body.helpers({
+	exclamation: function(){
+		return Session.get('exclamation');
+	},
+	adverb: function(){
+		return Session.get('adverb');
+	},
+	noun: function(){
+		return Session.get('noun');
+	},
+	verb: function(){
+		return Session.get('verb');
+	},
+});
+*/
 Template.body.events({
-	'click #submitButton': function(events){
+	'click #submitButton': function(event, template){
+		event.preventDefault();
 		console.log('Hello World..');
-		events.preventDefault();
+		exclamation = template.find('#exclamation').value;
+		adverb = template.find('#adverb').value;
+		noun = template.find('#noun').value;
+		verb = template.find('#verb').value;
+
+		console.log(exclamation + "! you say, as you " +adverb + 
+			" jump onto your " + noun + " and " + verb + " into the sunset.");
 	}
 });
